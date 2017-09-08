69283361b44824b185414609d09c1e51785ccbd7
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 21:42:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(arr){
   for (var i = 0; i < 25; i++){
-    //if (i === 1) arr = [...arr, `I am 1 strange loop.`];
-    //else arr = [...arr, `I am ${i + 1} strange loops.`];
-    return;
+    if (i === 1) arr = [...arr, `I am 1 strange loop.`];
+    else arr = [...arr, `I am ${i + 1} strange loops.`];
   }
+  return;
 }
 
 
