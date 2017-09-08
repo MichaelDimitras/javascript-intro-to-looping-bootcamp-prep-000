daf64764fb5dce37f9c474117734fcbeca5138d3
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 21:48:08 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(arr){
+  for (var i = 0; i < 25; i++){
+    if (i === 1) arr = [...arr, `I am 1 strange loop.`];
+    else arr = [...arr, `I am ${i} strange loops.`];
+  }
+  return arr;
+}
+
+
+function whileLoop(numTimes){
+  while(numTimes > 0){
+    console.log(numTimes);
+    numTimes--;
+  }
+  return "done";
+}
+
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(arr){
+  do {
+    arr.pop()
+  } while ( arr.length >= 0 && maybeTrue());
+  return arr;
+}
