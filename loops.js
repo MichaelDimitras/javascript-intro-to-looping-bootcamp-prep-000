62e15a9309e263c9d02bfe62afc22f45a06b17fa
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 21:35:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,4 +2,17 @@ function forLoop(arr){
   for (var i = 0; i < 25; i++){
     if (i === 1) arr = [...arr, `I am 1 strange loop.`];
     else arr = [...arr, `I am ${i+1} strange loops.`];
+    return;
+}
+
+
+function whileLoop(numTimes){
+  while(numTimes >= 0){
+    console.log(numTimes);
+    numTimes--;
+  }
+
+  console.log("Done!");
+
+  return;
 }
